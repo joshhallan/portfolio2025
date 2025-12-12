520ce1bef8a43a1496eacfea08f6b1b5903b364b
Josh Allan <joshuaallan@live.co.uk>
2025-12-12 09:23:02 +0000

    Removed commented code

--- src/components/HomePage/Experience/Experience.tsx
+++ src/components/HomePage/Experience/Experience.tsx
@@ -37,8 +37,7 @@ export default function ExperienceSummary() {
                 >
                     <div>
                         <h3 
-                            className="text-xl font-bold mb-1" 
-                            // style={{ color: cardColor, textShadow: `0 0 5px ${cardColor}` }}
+                            className="text-xl font-bold mb-1"
                             style={{ color: cardColor }}
                         >
                             {job.title}
