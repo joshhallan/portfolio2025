84a7d94c522210b41dc4d18c397a46710b70438e
Josh Allan <joshuaallan@live.co.uk>
2026-02-11 09:43:10 +0000

    Add Analytics component for Firebase integration and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import SkillsSection from "@/components/HomePage/Skills/Skills";
 import NeonDivider from "@/components/Global/Divder/Divider";
 import About from "@/components/HomePage/About/About";
 import ExperienceSummary from "@/components/HomePage/Experience/Experience";
+import Analytics from "@/components/Global/Analytics/Analytics";
 
 export default function Home() {
   return (
     <>
+      <Analytics />
+
       <Hero />
 
       <NeonDivider colourVariable="var(--neon-blue)" />
